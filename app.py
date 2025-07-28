d3a637af63efaadaad00299850d8ae89e3964fe1
AmirMahdi <168308022+amir7005@users.noreply.github.com>
2025-07-28 18:49:46 +0330

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,3 @@ def webhook():
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello from BTC bot!', 200
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
