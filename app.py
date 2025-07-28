a4ed5a75b1d50e6653fc96173a7368167fadbb4f
AmirMahdi <168308022+amir7005@users.noreply.github.com>
2025-07-28 18:53:13 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ def webhook():
 @app.route('/', methods=['GET'])
 def home():
     return 'Hello from BTC bot!', 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
